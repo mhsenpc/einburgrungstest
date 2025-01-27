003efe5ea0d9cf04fbaaa7c61b80e9ce6a9b771f
Mohsen Shamohammadi <mohsen.shamohammadi@joingsg.com>
2025-01-27 12:11:33 +0100

    add some questions

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,13 +1,35 @@
 export const questions = [
   {
+    text: "Welches Recht gehört zu den Grundrechten, die nach der deutschen Verfassung garantiert werden? Das Recht auf ...",
+    translation: "کدام یک از حقوق زیر ، که توسط قانون اساسی آلمان ضمانت شده، به حقوق بنیادی افراد متعلق است؟",
+    image: null,
+    answers: [
+      { id: 1, text: "Glaubens- und Gewissensfreiheit", translation: "آزادی مرام و عقیده", correct: true },
+      { id: 2, text: "Unterhaltung", translation: "آزادی تفریحات", correct: false },
+      { id: 3, text: "Arbeit", translation: "آزادی کار و اشتغال", correct: false },
+      { id: 4, text: "Wohnung", translation: "آزادی انتخاب مسکن", correct: false }
+    ]
+  },
+  {
+    text: "Wie heißt die deutsche Verfassung?",
+    translation: "نام قانون اساسی آلمان چیست؟",
+    image: null,
+    answers: [
+      { id: 1, text: "Volksgesetz", translation: "قانون مردم", correct: false },
+      { id: 2, text: "Bundesgesetz", translation: "قانون سراسری", correct: false },
+      { id: 3, text: "Deutsches Gesetz", translation: "قانون آلمان", correct: false },
+      { id: 4, text: "Grundgesetz", translation: "قانون اساسی", correct: true }
+    ]
+  },
+  {
     text: "In Deutschland dürfen Menschen offen etwas gegen die Regierung sagen, weil ...?",
     translation: "در آلمان مردم مجازند بدون واهمه نظرات خود را بر علیه دولت این کشور ابراز کنند، زیرا که",
     image: null,
     answers: [
-      { id: 1, text: "hier Religionsfreiheit gilt", translation: "در اینجا آزادی مذهب حکمفرماست", correct: true },
+      { id: 1, text: "hier Religionsfreiheit gilt", translation: "در اینجا آزادی مذهب حکمفرماست", correct: false },
       { id: 2, text: "die Menschen Steuern zahlen", translation: "مردم مالیات پرداخت میکنند", correct: false },
       { id: 3, text: "die Menschen das Wahlrecht haben", translation: "مردم از حق شرکت در انتخابات برخوردارند", correct: false },
-      { id: 4, text: "hier Meinungsfreiheit gilt, translation", translation: "در اینجا آزادی بیان حکمفرماست", correct: false }
+      { id: 4, text: "hier Meinungsfreiheit gilt", translation: "در اینجا آزادی بیان حکمفرماست", correct: true }
     ]
   },
   {
@@ -30,6 +52,72 @@ export const questions = [
       { id: 2, text: "Religionsunterricht teilnimmt", translation: "تعلیمات دینی", correct: true },
       { id: 3, text: "Politikunterricht teilnimmt", translation: "سیاست", correct: false },
       { id: 4, text: "Sprachunterricht teilnimmt", translation: "زبان", correct: false }
+    ]
+  },
+  {
+    text: "Welches Recht gehört zu den Grundrechten in Deutschland?",
+    translation: "کدامیک از حقوق زیر در آلمان جزو حقوق بنیادی افراد به شمار میاید؟",
+    image: null,
+    answers: [
+      { id: 1, text: "Waffenbesitz", translation: "مالکیت اسلحه", correct: false },
+      { id: 2, text: "Faustrecht", translation: "حق زورگویی", correct: false },
+      { id: 3, text: "Meinungsfreiheit", translation: "آزادی بیان", correct: true },
+      { id: 4, text: "Selbstjustiz", translation: "مجازات خودسرانه دیگران", correct: false }
+    ]
+  },
+  {
+    text: "Wahlen in Deutschland sind frei. Was bedeutet das?",
+    translation: "انتخابات عمومی در آلمان آزاد میباشد. مفهوم آن چیست؟",
+    image: null,
+    answers: [
+      { id: 1, text: "Man darf Geld annehmen, wenn man dafür einen bestimmten Kandidaten / eine bestimmte Kandidatin wählt.", translation: "انتخاب شخص خاصی به ازای اخذ پول مجاز است", correct: false },
+      { id: 2, text: "Nur Personen, die noch nie im Gefängnis waren, dürfen wählen.", translation: "صرفا افرادی که تاکنون به زندان نرفته اند، از حق رای برخوردارند", correct: false },
+      { id: 3, text: "Der Wähler darf bei der Wahl weder beeinflusst noch zu einer bestimmten Stimmabgabe gezwungen werden und keine Nachteile durch die Wahl haben.", translation: "رای دهنده نباید تحت تاثیر قرار داده، و یا به دادن رای خاصی مجبور، و نتیجتاً متحمل ضررهایی به جهت رای خود بشود", correct: true },
+      { id: 4, text: "Alle wahlberechtigten Personen müssen wählen.", translation: "کلیه افرادی که از حق رای برخوردار میباشند، باید در انتخابات شرکت کنن", correct: false }
+    ]
+  },
+  {
+    text: "Was steht nicht im Grundgesetz von Deutschland?",
+    translation: "کدامیک از موارد زیر در قانون اساسی آلمان درج نشده است ؟",
+    image: null,
+    answers: [
+      { id: 1, text: "Die Würde des Menschen ist unantastbar.", translation: "حرمت انسان قابل تعرض نیست", correct: false },
+      { id: 2, text: "Alle sollen gleich viel Geld haben.", translation: "همه باید به طور مساوی پول داشته باشند", correct: true },
+      { id: 3, text: "Jeder Mensch darf seine Meinung sagen.", translation: "همه از آزادی بیان برخوردارند", correct: false },
+      { id: 4, text: "Alle sind vor dem Gesetz gleich.", translation: "همه در مقابل قانون برابرهستند", correct: false }
+    ]
+  },
+  {
+    text: "Welches Grundrecht gilt in Deutschland nur für Ausländer / Ausländerinnen? Das Grundrecht auf ...",
+    translation: "کدامیک از حقوق بنیادی زیر صرفا برای خارجیان است؟",
+    image: null,
+    answers: [
+      { id: 1, text: "Schutz der Familie", translation: "حمایت از خانواده", correct: false },
+      { id: 2, text: "Menschenwürde", translation: "حرمت انسانی", correct: false },
+      { id: 3, text: "Asyl", translation: "پناهندگی", correct: true },
+      { id: 4, text: "Meinungsfreiheit", translation: "آزادی بیان", correct: false }
+    ]
+  },
+  {
+    text: "Was ist mit dem deutschen Grundgesetz vereinbar?",
+    translation: "کدامیک از مجازاتهای زیر با قانون اساسی آلمان مطابقت دارد؟",
+    image: null,
+    answers: [
+      { id: 1, text: "die Prügelstrafe", translation: "تنبیه بدنی", correct: false },
+      { id: 2, text: "die Folter", translation: "شکنجه", correct: false },
+      { id: 3, text: "die Todesstrafe", translation: "مجازات مرگ", correct: false },
+      { id: 4, text: "die Geldstrafe", translation: "مجازات نقدی", correct: true }
+    ]
+  },
+  {
+    text: "Wie wird die Verfassung der Bundesrepublik Deutschland genannt?",
+    translation: "قانون اساسی آلمان چه نامیده می شود؟",
+    image: null,
+    answers: [
+      { id: 1, text: "Grundgesetz", translation: "Grundgesetz", correct: true },
+      { id: 2, text: "Bundesverfassung", translation: "Bundesverfassung", correct: false },
+      { id: 3, text: "Gesetzbuch", translation: "Gesetzbuch", correct: false },
+      { id: 4, text: "Verfassungsvertrag", translation: "Verfassungsvertrag", correct: false }
     ]
   },
 ];
